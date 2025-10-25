c9a6cebb44ed574ed8effa356d668d498f00d97b
lyl-610520 <lyl610610610610610@outlook.com>
2025-10-26 03:45:55 +0800

    Update App.js

--- companion_frontend/src/App.js
+++ companion_frontend/src/App.js
@@ -20,6 +20,7 @@ import Settings from './components/Settings';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import GlobalPlayer from './components/GlobalPlayer';
+import FriendsPage from './components/friends/FriendsPage';
 
 
 // 您的主题创建逻辑 (保持不变)
